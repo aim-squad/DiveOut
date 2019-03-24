6f57028bd15a827c98321cc47807fb7367bbbe3c
pairving17 <pairving17.students.desu.edu>
2019-03-24 14:06:42 -0400

    Init Commit 3/24/19

---
+++ DiveOut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiveOut
+//
+//  Created by Philip Irivng  on 3/24/19.
+//  Copyright © 2019 AIM-Squad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
